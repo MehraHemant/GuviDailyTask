5b357c6ca184eba0b2b26237584eea3bd2647404
MehraHemant <hemantmehra9502@gmail.com>
2023-05-13 17:25:10 +0530

    Completed task 6, 7, 17, 18

---
+++ Day 7/OOPS task/person.js
@@ -0,0 +1,15 @@
+// Write a “person” class to hold all the details
+
+
+// declaring class
+class person{
+    // declaring object
+    constructor(firstName, lastName, age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+const person1 = new person('Hemant', 'Mehra', 24);
+console.log(person1);
